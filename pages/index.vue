d9dc2b10607ccec3792ce2af1a907c8d9e783db7
Zongwei_93 <soyayong@gmail.com>
2022-08-03 12:48:17 +0800

    Update homepages

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl">Welcome to My Portfolio</h1>
-      <p class="mt-5">Plan to design personal blog with portfolio</p>
+      <h3 class="text-5xl">I'm Yong Zong Wei</h3>
+      <p class="mt-5">Start from 2018 and enjoy cycling with my road bike -- 享受骑公路自行车的快感</p>
+      <img src="https://filedn.com/lzQz4YwK2qQSa8Al3y8Jwsk/Resume%20Image/cycling.jpg" alt="cycling" width="600" height="500">
     </div>
   </div>
 </template>
