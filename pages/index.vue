c7e1bcbf70e8b412a30e85bfc4b229e05236db32
Zongwei_93 <soyayong@gmail.com>
2022-08-04 21:08:43 +0800

    Update Homepages and static

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,52 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to My Portfolio</h1>
-      <h3 class="text-5xl">I'm Yong Zong Wei</h3>
-      <p class="mt-5">Start from 2018 and enjoy cycling with my road bike -- 享受骑公路自行车的快感</p>
-      <img src="https://filedn.com/lzQz4YwK2qQSa8Al3y8Jwsk/Resume%20Image/cycling.jpg" alt="cycling" width="600" height="500">
+  <div>
+    <div id="header" class="bg-[url('/hero-bg.jpg')] px-20">
+      <div id="menu" class="grid grid-cols-2 py-5">
+        <div id="menu-left">
+          <img src="logo.png" class="w-[150px]" alt="" />
+        </div>
+
+        <div id="menu-right">
+          <ul class="grid grid-cols-6 text-lg front-oswald text-white text-center uppercase pt-5">
+            <li>About Me</li>
+            <li>Education</li>
+            <li>Experience</li>
+            <li>Skills</li>
+            <li>Portfolio</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-2 pb-10 pt-10">
+        <img src="HaftBody.png" class="pr-2 mt-10" alt="" width="700" height="600" />
+        <div>
+          <h1 class="text-8xl text-white text-center font-lilita uppercase">
+            I'm Zong Wei
+          </h1>
+          <h2 class="text-6xl text-white text-center font-lilita uppercase my-5">
+            Hello and Welcome to My Webpage and Portfile
+          </h2>
+          <p class="text-white font-roboto font-light text-center">
+            Data Science | Data Analyst | Business Intelligence | Web Developer | AI | Machine Learning
+            | Deep Learning | React JS | React Native | Python | SQL | NoSQL
+            | Qlik View & Sense | Power BI | Tableau | LabVIEW
+          </p>
+        </div>
+      </div>
     </div>
+
+    <!-- <div id="section-1" class="text-center bg-[url('/three-burgers.jpg')] pb-60 bg-cover">
+      <h1 class="text-6xl uppercase font-oswald pt-20 px-24">
+        The burger tastes better when you eat it with family
+      </h1>
+      <p class="font-roboto font-light text-gray-500 text-lg py-6 px-28">
+        Spend quality time with your family and eat an affordable, nutritious and delicious meal together.
+        Not all burgers are fast foods. All our burgers are handmade to order for your family members to enjoy.
+      </p>
+      <button class="font-oswald uppercase bg-red-500 text-white py-3 px-8">
+        Explore Full Menu
+      </button>
+    </div> -->
   </div>
 </template>
