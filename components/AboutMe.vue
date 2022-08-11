7e92a25604e6f0a047664fa94b5584b19867b314
Zongwei_93 <soyayong@gmail.com>
2022-08-11 11:34:19 +0800

    Update AboutMe

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,49 +1,51 @@
 <template>
     <div class="grid grid-cols-3 pb-5 pt-5">
-        <img src="FullBody.png" class="px-2 mt-10" alt="" width="400" height="400" />
-        <div class="col-span-2">
+        <div>
+            <img src="FullBody.png" class="px-2 mt-5 mx-20 object-center" alt="" width="400" height="400" />
+            <a class="font-oswald uppercase bg-blue-500 hover:bg-blue-700 text-white py-3 px-8 rounded" href="mailto:soyayong@gmail.com">
+                    Hire Me
+            </a>
+        </div>
+        <div class="col-span-2 mx-5">
             <h1 class="text-6xl uppercase font-oswald pt-20 px-24">
                 About Me
             </h1>
             <h6 class="text-5xl uppercase font-oswald pt-20 px-24">
                 A small introduction about myself
             </h6>
-            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-10">
+            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-5">
                 Business Intelligence Developer at GAT System Sdn.Bhd on 11 April 2022.
             </p>
-            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-10">
+            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-5">
                 I was born on 10 April 1993 in Kuala Pilah, Negeri Sembilan, Malaysia,
                 and Bachelor of Mechatronic Engineering at University Malaysia Pahang,
                 Pekan, Pahang in 2017 June. Currently, work as
                 Business Intelligence Developer at GAT System Sdn.Bhd.
                 (THE VERTICAL, BLOCK A, 23A-05, Jalan Kerinchi,Bangsar South, 59200, Kuala Lumpur, Malaysia.)
             </p>
-            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-10">
+            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-5">
                 Experienced as a full-stack developer using LabVIEW, Data Engineer like ETL skill, indexes, query, and performance in MSSQL.
                 Besides that, a data analyst skill set applies and uses BI tools like Qlik Sense and Power BI for visualization in the
                 Shimano company from 2017 until 2021 August. In 2021 August a data science career path under MC Crenergy company as a contract base.
                 While 2022 joined my current company as BI developer.
             </p>
-            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-10">
+            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-5">
                 Based on those experiences that I pass through, I can say that I'm quite good in those skills like LabVIEW,
                 SQL(query, indexes, performance tuning, and dynamic SQL), Master Data Management,
                 BI Tools(Qlik Sense started in 2018, PowerBI started in 2019, and Tableau started in 2021),
                 Python, ETL tools, and basic javascript.
             </p>
-            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-10">
+            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-5">
                 Apart from that, I found that self-education is most important to growing my knowledge.
                 So starting from 2018 to improve developer skills such as SQL Server, Data Science,
                 Web Development (Javascript and ReactJS), and BI tools.
                 My future career plan is to become a full stack in data science.
             </p>
-            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-10">
+            <p class="font-roboto font-light text-gray-500 text-lg py-6 px-5">
                 Lastly, the most important thing is to apply soft skills like self-motivation,
                 teamwork, responsibility, problem-solving, communication, leadership,
                 able to work under pressure, and well time management in my life or working area.
             </p>
-            <a class="font-oswald uppercase bg-blue-500 hover:bg-blue-700 text-white py-3 px-8 rounded" href="mailto:soyayong@gmail.com">
-                Hire Me
-            </a>
         </div>
     </div>
 </template>
